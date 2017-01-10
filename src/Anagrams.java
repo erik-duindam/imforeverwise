5b5c5581a98b2f266f1086bb98f75d0c75c94396
Erik Duindam <erik@erikduindam.com>
2017-01-09 23:00:00 -0500

    Docs

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Anagrams {
 
     /**
-     * Please ignore this function and look at the explanations below, and run the tests (JUnit 4).
+     * Please ignore this function and look at the explanations below, and possibly run the tests (JUnit 4).
      *
      * @param args
      */
@@ -22,8 +22,8 @@ public class Anagrams {
      *
      * There are two downsides to this approach:
      *  1. A lot of natural words contain the same (starting) characters, which means the HashMap will have a lot of
-     *     redundant starting characters that take space. This means you'll need a lot of memory for large maps.
-     *  2. HashMaps cannot use primitive values (like ints/chars) as key, which means there's quite a lot of memory
+     *     redundant starting characters that take space. This means you'll need quite a bit of memory for large maps.
+     *  2. Java HashMaps cannot use primitive values (like ints/chars) as keys, which means there's quite a lot of memory
      *     overhead due to the fact that Java has to store objects on the heap.
      */
     public static void explainHashmapSolution() {
@@ -37,10 +37,12 @@ public class Anagrams {
     }
 
     /**
-     * An alternative way to find anagrams is to create an alphabetically ordered Trie. The main benefit is saving some
+     * An alternative way to find anagrams is to create an alphabetically ordered Trie. The main benefit is saving
      * space for the lookup keys, and thus for memory. This method would be a lot more beneficial if we were only interested
-     * in the number of anagrams (the count) instead of the actual words. To further optimize this, radix tries would be
-     * even better.
+     * in the number of anagrams (the count) instead of the actual words. We can further optimize memory usage by
+     * implementing radix tries instead of plain tries. Additionally, we can strongly optimize the solution by limiting
+     * the input data to a predefined set of characters (i.e. 26 characters of the alphabet) and use plain arrays with
+     * integer indexes to store the actual words.
      *
      * The main downside of this approach is the O(log n) lookup and insert time.
      */
@@ -57,23 +59,24 @@ public class Anagrams {
     /**
      * If you would have a set of hundreds of millions of dictionary words, storing all of these in memory is not going
      * to be efficient (or even possible). If you'd still want to use a manual HashMap or Trie approach, you'd have to
-     * store separate maps/tries based on the input word length, or based on the first characters. You would end up with
+     * persist separate maps/tries based on the input word length, or based on the first characters. You would end up with
      * many smaller maps or tries instead of 1 big one. It's conceptually the same as creating a database index.
      * However, there's no point in building this manually, since there are great tools out there for database indexing.
      *
      * The better solution is to use proven database software. If the set is indeed 100M records, any decent database system
-     * should still be able to handle this on one server without sharding. It wouldn't matter that much if you'd use
-     * MySQL, PostgreSQL, MongoDB, etc. You would create a table or collection with an INDEX on the alphabetical key.
+     * should still be able to handle this on one server without sharding. It wouldn't matter that much whether you'd use
+     * MySQL, PostgreSQL, MongoDB or something else. You would create a table or collection with an INDEX on the alphabetical key.
      *
-     * A more complicated question would be how you would get the anagrams of a word with a 10B word dictionary,
-     * or a 100M word dictionary that's being updated every second. In that case, you'd need a database system that's able
-     * to handle a lot of reads and writes while scaling over multiple servers. You could theoretically do this with
-     * any database system by using key-based sharding and master-slave replication, but it would still be tricky for
-     * something like PostgreSQL if there are billions of records.
+     * A more complicated question would be how you would scale this to a 10B word dictionary, or a 100M word dictionary
+     * that's being updated every second. In that case, you'd need a database system that's able to handle a lot of reads
+     * and writes while scaling over multiple servers. Due to the O(1) lookup nature of anagrams, you might still be able
+     * to do this on any common database system. But, if you want to scale this even further, you'll have to find a database
+     * system that's capable of handling billions of records without effort.
      *
-     * One great tool for this use case would be a column store like Cassandra. Cassandra partitions data over multiple
-     * nodes (servers) automatically based on your partition key per table. If you use the anagram key's length and first
-     * characters in the partition key, Cassandra will automatically partition your anagrams over many servers.
+     * One great tool for this would be a column store like Cassandra. Cassandra partitions data over multiple
+     * nodes (servers) automatically based on your partition key per table. If we'd use the anagram key's length and first
+     * characters in the partition key, we can partition our dictionary distributed on many servers without manual sharding
+     * or manual replication overhead.
      *
      * Cassandra model:
      *  CREATE TABLE anagrams (
@@ -91,15 +94,18 @@ public class Anagrams {
      * Example selection:
      *  SELECT * FROM anagrams WHERE length = 4 AND first_char = 'e' AND a_key = 'eikr';
      *
-     * The partition key is (length, first_char), which means the keys of anagrams are split up in all combinations of
+     * The partition key is (length, first_char), which means the partitions of anagrams are split up in all combinations of
      * key length and first character. Assuming around 26 alphabet characters and a max word length of 35, the keys
-     * are partitioned over 26 x 35 = 910 partitions. Cassandra can write each partition on a different server, with
-     * replication on a per-partition basis. So practically you'd have each partition written on at least 3 nodes.
+     * are partitioned over 26 x 35 = 910 partitions. Cassandra could write each partition on a different server, with
+     * replication on a per-partition basis. This is potentially scalable over hundreds or even thousands of servers.
      *
      * Cassandra is able to do 1M reads/writes per second on a pretty common server setup (3 relatively large servers).
      * Given the fact that we break up our problem in (max) 910 parts, we should be able to scale this to Facebook
-     * proportions. If 910 parts is not enough, we could partition on the first 2 characters instead, which would give as a huge
-     * number of extra partitions.
+     * proportions. If 910 parts is not enough, we could partition on the first 2 characters instead, which would give
+     * us a huge number of extra partitions. In all of these cases, our select queries would be in the order of tens
+     * or hundreds of ms.
+     *
+     * Alternatively, we can just use a cloud service like DynamoDB :-)
      */
     public static void explain100MSolution() {
 
